26110cea0b31aae0c92e2c2dde3860c6d416b153
starlite-jack <90665508+starlite-jack@users.noreply.github.com>
2024-07-28 17:05:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,47 +2,22 @@ function button1() {
     var input = document.getElementById("inputfieldjs").value;
     var links = input.split(/\r?\n/); 
     for (var i = 0; i < links.length; i++) {
-      var trimmedLink = links[i].trim();
-      if (trimmedLink) {
-        if (!trimmedLink.match(/(https?:\/\/)/)) {
-          trimmedLink = 'https://archive.ph/?run=1&url=' + trimmedLink; 
+        var trimmedLink = links[i].trim();
+        if (trimmedLink) {
+            if (!trimmedLink.match(/(https?:\/\/)/)) {
+                trimmedLink = 'https://archive.ph/?run=1&url=' + encodeURIComponent(trimmedLink);
+            } else {
+                trimmedLink = 'https://archive.ph/?run=1&url=' + encodeURIComponent(trimmedLink);
+            }
+            try {
+                window.open(trimmedLink, '_blank');
+            } catch (error) {
+                alert('Failed to open link: ', trimmedLink, error);
+            }
         }
-        window.open(trimmedLink, '_blank');
-      }
     }
-  } 
-  
-  function isMobileDevice() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    let textarea = document.getElementById('inputfieldjs');
-  
-    if (textarea) {
-      textarea.addEventListener('keydown', (e) => {
-        if (e.keyCode === 13 && !e.shiftKey && !isMobileDevice()) { 
-          e.preventDefault(); 
-          button1();
-        }
-      });
-    }
-  });
-  
-  if (isMobileDevice()) {
-  }
-  
-  function clearthetext(){
-      document.getElementById('inputfieldjs').value = ''
-  }
-  
-  function pastetext() {
-      var textarea = document.querySelector('textarea');
-      navigator.clipboard.readText()
-          .then(text => {
-              textarea.value = text;
-          })
-          .catch(err => {
-              prompt("looks like we have encountered an error\nplease copy and search the error below", err)
-          });
-  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+});
